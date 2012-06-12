b0b532d112c8792897c007b1cec3c5f7eaf447d9
tony <code@getbraintree.com>
2012-06-12 17:01:24 -0500

    passes along query params and post body

--- lib/mallory/proxy.rb
+++ lib/mallory/proxy.rb
@@ -1,27 +1,31 @@
 module Mallory
   class Proxy < Sinatra::Base
     post "/*" do
-      _proxy_request(:post, env["PATH_INFO"])
+      _proxy_request(:post, env["PATH_INFO"], env["QUERY_STRING"], env["rack.request.form_vars"])
     end
 
     get "/*" do
-      _proxy_request(:get, env["PATH_INFO"])
+      _proxy_request(:get, env["PATH_INFO"], env["QUERY_STRING"])
     end
 
     put "/*" do
-      _proxy_request(:put, env["PATH_INFO"])
+      _proxy_request(:put, env["PATH_INFO"], env["QUERY_STRING"])
     end
 
     delete "/*" do
-      _proxy_request(:delete, env["PATH_INFO"])
+      _proxy_request(:delete, env["PATH_INFO"], env["QUERY_STRING"])
     end
 
-    def _proxy_request(http_method, path)
+    def _proxy_request(http_method, path, query, body = nil)
       uri = URI.join(Mallory.target, path)
-      request_options = {}
-      request_options[:tls] = {:verify_peer => true, :cert_chain_file => Mallory.ca_file} if uri.scheme == "https"
+      uri.query = query
+      connect_options = {}
+      connect_options[:tls] = {:verify_peer => true, :cert_chain_file => Mallory.ca_file} if uri.scheme == "https"
 
-      response = EM::HttpRequest.new(uri, request_options).send(http_method)
+      request_options = {}
+      request_options[:body] = body unless body.nil?
+
+      response = EM::HttpRequest.new(uri, connect_options).send(http_method, request_options)
       status = response.state == :finished ? response.response_header.status : 503
       [status, response.response_header, response.response]
     end
--- spec/mallory/proxy_spec.rb
+++ spec/mallory/proxy_spec.rb
@@ -39,7 +39,7 @@ describe Mallory::Proxy do
       Mallory.ca_file = File.expand_path("../ssl/ca-certificates.crt", File.dirname(__FILE__))
 
       post "/foo"
-      last_response.body.should == "POST: /foo"
+      last_response.body.should == "POST: /foo  "
       last_response.status.should == 200
     end
   end
@@ -48,31 +48,47 @@ describe Mallory::Proxy do
     it "POST proxies the traffic to the backend server" do
       post "/foo"
 
-      last_response.body.should == "POST: /foo"
+      last_response.body.should == "POST: /foo  "
       last_response.status.should == 200
     end
 
     it "GET proxies the traffic to the backend server" do
       get "/foo"
 
-      last_response.body.should == "GET: /foo"
+      last_response.body.should == "GET: /foo  "
       last_response.status.should == 200
     end
 
     it "PUT proxies the traffic to the backend server" do
+      EM.stop
       pending "WEBrick does not support put"
       put "/foo"
 
-      last_response.body.should == "PUT: /foo"
+      last_response.body.should == "PUT: /foo  "
       last_response.status.should == 200
     end
 
     it "DELETE proxies the traffic to the backend server" do
+      EM.stop
       pending "WEBrick does not support delete"
       delete "/foo"
 
-      last_response.body.should == "DELETE: /foo"
+      last_response.body.should == "DELETE: /foo  "
       last_response.status.should == 200
     end
+  end
+
+  it "passes along query params" do
+    post "/query?a=b&c=d"
+
+    last_response.body.should == "POST: /query a=b&c=d "
+    last_response.status.should == 200
+  end
+
+  it "passes along post body" do
+    post "/body", "<xml>foo</xml>"
+
+    last_response.body.should == "POST: /body  <xml>foo</xml>"
+    last_response.status.should == 200
   end
 end
