db21a6d402c1b36f847fb07cdd4ce87207a58ca6
dan and tony <code@getbraintree.com>
2012-06-13 16:24:23 -0500

    configurable proxy destination for mallory request handler

--- mallory/request_handler.py
+++ mallory/request_handler.py
@@ -8,21 +8,24 @@ import sys
 
 class RequestHandler(tornado.web.RequestHandler):
 
-    def initialize(self, proxy_to = None):
+    def initialize(self, proxy_to, ca_file):
         self.proxy_to = proxy_to
-        print "proxying to %s" % proxy_to
+        self.ca_file = ca_file
+        print "proxying to %s with %s" % (proxy_to, ca_file)
 
     @tornado.web.asynchronous
     @tornado.gen.engine
     def get(self):
         try:
-            print "proxying to %s" % self.proxy_to
+            print "proxying to %s with %s" % (self.proxy_to, self.ca_file)
             outbound_request = tornado.httpclient.HTTPRequest(
                 self.proxy_to,
+                ca_certs = self.ca_file,
                 method="GET"
             )
             http_client = tornado.httpclient.AsyncHTTPClient(io_loop=tornado.ioloop.IOLoop.instance())
             #print "yielding"
+
             response = yield tornado.gen.Task(http_client.fetch, outbound_request)
             print response
 
