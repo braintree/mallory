ff5cb8843b100a7a1b162f86d0b6a21e84bac20a
dan and tony <code@getbraintree.com>
2012-06-14 13:31:39 -0500

    write a pid file

--- mallory/cli.py
+++ mallory/cli.py
@@ -1,9 +1,11 @@
-import optparse
+import logging
 import mallory
 import mallory.logs
-import tornado.ioloop
+import optparse
+import os
 import signal
 import sys
+import tornado.ioloop
 
 def start(args):
     parser = optparse.OptionParser(usage="mallory [options] target")
@@ -11,21 +13,29 @@ def start(args):
     parser.add_option("--verify-ca-cert", dest="ca_cert", help="CA certificate to verify on backend")
     parser.add_option("--ssl-key", dest="ssl_key", help="Private key for serving SSL")
     parser.add_option("--ssl-cert", dest="ssl_cert", help="Certificate for serving SSL")
+    parser.add_option("--pid-file", dest="pid_file", help="Pid file")
 
     (options, params) = parser.parse_args(args)
-
 
     mallory.logs.setup()
     server = mallory.Server(proxy_to=params[0], port=options.port, ca_file=options.ca_cert, ssl_options={ "certfile": options.ssl_cert, "keyfile": options.ssl_key })
     handle_interrupt(server)
 
+    logging.info("starting mallory on port %s" % options.port)
     server.start()
+
+    write_pid_file(options.pid_file)
+
     tornado.ioloop.IOLoop.instance().start()
+
+def write_pid_file(file_location):
+    with open(file_location, 'w') as f:
+        f.write(str(os.getpid()))
 
 def handle_interrupt(server):
     def signal_handler(signal, frame):
         server.stop()
-        print 'You pressed Ctrl+C!'
+        logging.info('stopping mallory...')
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
