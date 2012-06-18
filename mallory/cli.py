189b999093a96454ae51aac9f37df342cc11c79a
tony <code@getbraintree.com>
2012-06-18 09:21:27 -0500

    removes pid file option since we aren't daemonizing

--- mallory/cli.py
+++ mallory/cli.py
@@ -13,7 +13,6 @@ def start(args):
     parser.add_option("--verify-ca-cert", dest="ca_cert", help="CA certificate to verify on backend")
     parser.add_option("--ssl-key", dest="ssl_key", help="Private key for serving SSL")
     parser.add_option("--ssl-cert", dest="ssl_cert", help="Certificate for serving SSL")
-    parser.add_option("--pid-file", dest="pid_file", help="Pid file")
     parser.add_option("--proxy-request-timeout", dest="proxy_request_timeout", default=20, type="float", help="Proxy timeout in seconds")
 
     (options, params) = parser.parse_args(args)
@@ -25,13 +24,7 @@ def start(args):
     logging.info("starting mallory on port %s" % options.port)
     server.start()
 
-    write_pid_file(options.pid_file)
-
     tornado.ioloop.IOLoop.instance().start()
-
-def write_pid_file(file_location):
-    with open(file_location, 'w') as f:
-        f.write(str(os.getpid()))
 
 def handle_interrupt(server):
     def signal_handler(signal, frame):
