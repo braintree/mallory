731317222506e934c9bf4916d9a8338f7d0ac05e
ali <code@getbraintree.com>
2012-08-13 13:37:26 -0700

    use mallory_client in tests

--- test/mallory_test.py
+++ test/mallory_test.py
@@ -17,7 +17,7 @@ import test
 class MalloryTest(tornado.testing.AsyncTestCase):
     def setUp(self):
         super(MalloryTest, self).setUp()
-        self.http_client = tornado.httpclient.AsyncHTTPClient()
+        self.client = test.MalloryClient(10001, self)
 
         echo_app = tornado.web.Application([
             (r"/.*", test.EchoRequestHandler)
@@ -40,13 +40,11 @@ class MalloryTest(tornado.testing.AsyncTestCase):
         return 'https://127.0.0.1:%s%s' % (10001, path)
 
     def test_a_200_response_on_hitting_the_root_url(self):
-        self.http_client.fetch(self.get_url("/"), self.stop, ca_certs = "test/ssl/mallory/server.crt")
-        response = self.wait()
+        response = self.client.get("/", self)
         self.assertEqual(200, response.code)
 
     def test_path_is_proxied(self):
-        self.http_client.fetch(self.get_url("/the/path/to/hit"), self.stop, ca_certs = "test/ssl/mallory/server.crt")
-        response = self.wait()
+        response = self.client.get("/the/path/to/hit", self)
         self.assertTrue(response.body.find("PATH: /the/path/to/hit") >= 0)
 
     def test_query_string_is_not_logged(self):
@@ -54,8 +52,7 @@ class MalloryTest(tornado.testing.AsyncTestCase):
         log_handler = logging.StreamHandler(log_output)
         logging.getLogger().addHandler(logging.StreamHandler(log_output))
 
-        self.http_client.fetch(self.get_url("/path?queryparam=value"), self.stop, ca_certs = "test/ssl/mallory/server.crt")
-        response = self.wait()
+        response = self.client.get("/path?queryparam=value", self)
 
         logging.getLogger().removeHandler(log_handler)
 
@@ -66,54 +63,54 @@ class MalloryTest(tornado.testing.AsyncTestCase):
         self.assertEqual(False, log_output.getvalue().find("GET queryparam=value") >= 0)
 
     def test_query_params_are_proxied(self):
-        self.http_client.fetch(self.get_url("/path?param1=value1&param2=value2"), self.stop, ca_certs = "test/ssl/mallory/server.crt")
-        response = self.wait()
+        response = self.client.get("/path?param1=value1&param2=value2", self)
         self.assertTrue(response.body.find("QUERY STRING: param1=value1&param2=value2") >= 0)
 
     def test_http_status_code_is_returned_in_the_response(self):
-        self.http_client.fetch(self.get_url("/http_status/201"), self.stop, ca_certs = "test/ssl/mallory/server.crt")
-        response = self.wait()
+        response = self.client.get("/http_status/201", self)
         self.assertEqual(201, response.code)
 
     def test_http_headers_are_passed_along(self):
-        self.http_client.fetch(self.get_url("/"), self.stop, ca_certs = "test/ssl/mallory/server.crt", headers = { "X-Something": "bar" })
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client.fetch(self.get_url("/"), self.stop, ca_certs = "test/ssl/mallory/server.crt", headers = { "X-Something": "bar" })
         response = self.wait()
         self.assertTrue(response.body.find("X-Something: bar") >= 0, response.body)
 
     def test_host_header_is_not_passed(self):
-        self.http_client.fetch(self.get_url("/"), self.stop, ca_certs = "test/ssl/mallory/server.crt")
-        response = self.wait()
+        response = self.client.get("/", self)
         self.assertTrue(response.body.find("Host: 127.0.0.1:10000") >= 0, response.body)
 
     def test_response_headers_are_returned(self):
-        self.http_client.fetch(self.get_url("/http_header/X-CustomHeader/header-value"), self.stop, ca_certs = "test/ssl/mallory/server.crt")
-        response = self.wait()
+        response = self.client.get("/http_header/X-CustomHeader/header-value", self)
         self.assertEqual('header-value', response.headers['X-CustomHeader'])
 
     def test_post_body_is_passed_along(self):
-        self.http_client.fetch(self.get_url("/http_header/X-CustomHeader/header-value"), self.stop, method = "POST", body = "the post body", ca_certs = "test/ssl/mallory/server.crt")
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client.fetch(self.get_url("/http_header/X-CustomHeader/header-value"), self.stop, method = "POST", body = "the post body", ca_certs = "test/ssl/mallory/server.crt")
         response = self.wait()
         self.assertTrue(response.body.find("METHOD: POST") >= 0, response.body)
         self.assertTrue(response.body.find("BODY: the post body") >= 0, response.body)
 
     def test_sets_x_proxy_server_in_response(self):
-        self.http_client.fetch(self.get_url("/"), self.stop, ca_certs = "test/ssl/mallory/server.crt")
-        response = self.wait()
+        response = self.client.get("/", self)
         self.assertEqual(socket.gethostname(), response.headers['X-Proxy-Server'])
 
     def test_put(self):
-        self.http_client.fetch(self.get_url("/"), self.stop, method = "PUT", body = "the put body", ca_certs = "test/ssl/mallory/server.crt")
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client.fetch(self.get_url("/"), self.stop, method = "PUT", body = "the put body", ca_certs = "test/ssl/mallory/server.crt")
         response = self.wait()
         self.assertTrue(response.body.find("METHOD: PUT") >= 0, response.body)
         self.assertTrue(response.body.find("BODY: the put body") >= 0, response.body)
 
     def test_delete(self):
-        self.http_client.fetch(self.get_url("/"), self.stop, method = "DELETE", ca_certs = "test/ssl/mallory/server.crt")
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client.fetch(self.get_url("/"), self.stop, method = "DELETE", ca_certs = "test/ssl/mallory/server.crt")
         response = self.wait()
         self.assertTrue(response.body.find("METHOD: DELETE") >= 0, response.body)
 
     def test_head(self):
-        self.http_client.fetch(self.get_url("/"), self.stop, method = "HEAD", ca_certs = "test/ssl/mallory/server.crt")
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client.fetch(self.get_url("/"), self.stop, method = "HEAD", ca_certs = "test/ssl/mallory/server.crt")
         response = self.wait()
         self.assertEqual("HEAD", response.headers["X-Requested-Method"])
 
@@ -121,7 +118,8 @@ class MalloryTest(tornado.testing.AsyncTestCase):
         mallory_server = mallory.Server(proxy_to="https://127.0.0.1:10001", ca_file="test/ssl/badguy.crt", port=10002, request_timeout=5,ssl_options =  { "certfile": "test/ssl/mallory/server.crt", "keyfile": "test/ssl/mallory/server.key" })
         mallory_server.start()
 
-        self.http_client.fetch("https://127.0.0.1:10002/", self.stop, method = "HEAD", ca_certs = "test/ssl/mallory/server.crt")
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client.fetch("https://127.0.0.1:10002/", self.stop, method = "HEAD", ca_certs = "test/ssl/mallory/server.crt")
         response = self.wait()
 
         mallory_server.stop()
@@ -129,12 +127,14 @@ class MalloryTest(tornado.testing.AsyncTestCase):
         self.assertEqual(502, response.code)
 
     def test_timeout(self):
-        self.http_client.fetch(self.get_url("/timeout"), self.stop, request_timeout=3, ca_certs = "test/ssl/mallory/server.crt")
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client.fetch(self.get_url("/timeout"), self.stop, request_timeout=3, ca_certs = "test/ssl/mallory/server.crt")
         response = self.wait()
         self.assertEqual(502, response.code)
         self.assertEqual(socket.gethostname(), response.headers['X-Proxy-Server'])
 
     def test_gzipped_response(self):
-        self.http_client.fetch(self.get_url("/gzip"), self.stop, request_timeout=3, ca_certs = "test/ssl/mallory/server.crt")
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client.fetch(self.get_url("/gzip"), self.stop, request_timeout=3, ca_certs = "test/ssl/mallory/server.crt")
         response = self.wait()
         self.assertEqual(200, response.code)
